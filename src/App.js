e962c31e305ee9e31fd8d9a840a4411773a18d72
surajjayraman <suraj.jayraman@gmail.com>
2023-06-14 10:43:39 -0400

    Update App component with Dynamic events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Intro1 from "./components/Intro1";
+import Intro2 from "./components/Intro2";
+import Intro3 from "./components/Intro3";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function handleClick() {
+   let randomNum = Math.floor(Math.random() * 3) + 1;
+   console.log(randomNum);
+   let userInput = prompt('type a number');
+   alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+ 
+ return (
+   <div>
+     <h1>Task: Add a button and handle a click event</h1>
+     <button onClick={handleClick}>Guess the number between 1 and 3.</button>
+     <Intro1 />
+     <Intro2 />
+     <Intro3 />
+   </div>
+ );
 }
 
 export default App;
