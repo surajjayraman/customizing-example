8f8cf7af366d1c95fc8070671a6577a9e8a6c7b1
surajjayraman <suraj.jayraman@gmail.com>
2023-06-16 18:09:18 -0400

    Add low calorie Dessert component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,31 @@ import Counter from "./components/Counter";
 import { Routes, Route, Link } from 'react-router-dom';
 import LogicalAnd from "./components/LogicalAnd";
 import MyVideo from "./components/MyVideo";
+import DessertsList from "./components/DessertsList";
 
+// Desserts Data
+const desserts = [
+  {
+    name: "Chocolate Cake",
+    calories: 400,
+    createdAt: "2022-09-01",
+  },
+  {
+    name: "Ice Cream",
+    calories: 200,
+    createdAt: "2022-01-02",
+  },
+  {
+    name: "Tiramisu",
+    calories: 300,
+    createdAt: "2021-10-03",
+  },
+  {
+    name: "Cheesecake",
+    calories: 600,
+    createdAt: "2022-01-04",
+  },
+];
 function App() {
   function handleClick() {
    let randomNum = Math.floor(Math.random() * 3) + 1;
@@ -62,6 +86,10 @@ function App() {
     <div>
       <h2>React Media Demo</h2>
         <MyVideo />
+    </div>
+    <div className="App">
+      <h2>List of low calorie desserts:</h2>
+      <DessertsList data={desserts} />
     </div>  
    </div>
  );
