4d6dd33c89cc10496b53efbea5bbfc860bf85aea
surajjayraman <suraj.jayraman@gmail.com>
2023-06-17 14:09:59 -0400

    Update Form component with uncontrolled input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 const Form = () => { 
     const inputRef = useRef(null);
+    const [input, setInput] = useState('');
 
     const handleSubmit = (e) => {
         const inputValue = inputRef.current.value;
@@ -8,10 +9,16 @@ const Form = () => {
         e.preventDefault();
     }
 
+    const handleChange = (e) => {
+        setInput(e.target.value);
+        e.preventDefault();
+    }
+
 
     return ( 
       <form onSubmit={handleSubmit}> 
         <input ref={inputRef} type="text" /> 
+        <input value={input} onChange={handleChange} type='text' />
       </form> 
     ); 
    }; 
